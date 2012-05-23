60cba8563b358393182500c0ea07c742b47cb0a1
Ruslan Popov <ruslan.popov@gmail.com>
2012-05-24 00:45:29 +0400

    Отвязал декоратор от функционала кошелька.

--- src/storage/decorators.py
+++ src/storage/decorators.py
@@ -8,14 +8,30 @@ from django.utils.hashcompat import sha_constructor
 
 
 def cache_factory(key_tpl, timeout):
+    u"""
+    Возвращает декоратор кэширования.
+
+    @type  key_tpl: unicode
+    @param key_tpl: Шаблон ключа для размещения в кэше.
+    @type  timeout: integer
+    @param timeout: Время кэширования в секундах.
+
+    @rtype: callable
+    @return: Декоратор кэширования.
+    """
     def decorator(func):
         @wraps(func)
-        def wrapped(wallet_type):
-            key_raw = key_tpl % wallet_type
+        def wrapped(*args, **kwargs):
+
+            def _params_to_hash(*a, **k):
+                return reduce(lambda x, y: x + y,
+                    map(unicode, list(a) + k.values()))
+
+            key_raw = key_tpl % _params_to_hash(*args, **kwargs)
             key = sha_constructor(key_raw.encode('utf-8')).hexdigest()
             value = cache.get(key)
             if value is None:
-                value = func(wallet_type)
+                value = func(*args, **kwargs)
                 cache.set(key, value, timeout)
             return value
         return wrapped
