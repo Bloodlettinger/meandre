fbdb5a3dc567297f7dbf9240e9bb0829e0b71ec6
Ruslan Popov <ruslan.popov@gmail.com>
2012-08-13 18:58:11 +0400

    Дамп БД перед импортом конфигурации.

--- import_production.sh
+++ import_production.sh
@@ -1,5 +1,7 @@
 source ./.production
 
+ssh ${USER}@${HOST} "mysqldump -h ${DBHOST} -u ${DBUSER} -p${DBPASS} ${DBNAME} > ~/site1/dump.sql"
+
 rsync --verbose --archive --delete ${USER}@${HOST}:site1/src/public/media ./src/public/
 rsync --verbose --archive --delete ${USER}@${HOST}:site1/dump.sql ./tmp/
 
