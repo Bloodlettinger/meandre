d42570d60a5c89c36dd6e188f9537956e812c814
Ruslan Popov <ruslan.popov@gmail.com>
2012-09-10 17:46:32 +0400

    Конфликт имён в системе развёртывания.

--- fabfile/hosts.py
+++ fabfile/hosts.py
@@ -5,7 +5,7 @@ from src import local_settings as settings
 
 COMMON_OPTIONS = dict(
     ENV_DIR=settings.ENV_DIR,
-    PROJECT_DIR=settings.PROJECT_DIR,
+    PROJECT_DIR=settings.HOSTING_DIR,
     DB_HOST=settings.DB_HOST,
     DB_NAME=settings.DB_NAME,
     DB_USER=settings.DB_USER,
